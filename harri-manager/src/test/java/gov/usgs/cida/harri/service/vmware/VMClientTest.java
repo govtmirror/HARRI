c3b79c5516cf9dc360e04e02d2664ddb3f478825
mbucknel <mbucknel@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2013-01-23 21:09:36 +0000

    Added gov.usgs.cida.harri.vmware.SimpleClient which was a sample program that came with vSphere. This program works when the correct password is entered.
    Added gov.usgs.cida.harri.vmware.VMClient which provides a class to instantiate with the url, username, and password to access the list of virtual machines. This code is not working. I have written a test class for this.

---
+++ harri-manager/src/test/java/gov/usgs/cida/harri/service/vmware/VMClientTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.usgs.cida.harri.service.vmware;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mbucknel
+ */
+public class VMClientTest {
+
+    public VMClientTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getVirtualMachines method, of class VMClient.
+     */
+    @Test
+    public void testGetVirtualMachines() throws Exception {
+        System.out.println("getVirtualMachines");
+        VMClient instance = new VMClient("https://cida-eros-vco.er.usgs.gov/sdk/vimService", "harri", "XXXXXX");//TODO put in real password for demo
+        List<String> result = instance.getVirtualMachines();
+        for (String s: result){
+            System.out.println(s);
+        }
+    }
+}
