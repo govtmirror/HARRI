7ea85893fb80715df25ce39ae46f308a1441b1d3
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2013-03-26 17:44:18 +0000

    Added a bit of functionality to couch dao

--- harri-couch-dao/src/main/java/gov/usgs/cida/harri/couchdbdao/HarriCouchDBDao.java
+++ harri-couch-dao/src/main/java/gov/usgs/cida/harri/couchdbdao/HarriCouchDBDao.java
@@ -1,8 +1,11 @@
 package gov.usgs.cida.harri.couchdbdao;
 
 import gov.usgs.cida.harri.commons.interfaces.dao.IHarriDAO;
+import gov.usgs.cida.harri.util.HarriUtils;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -11,9 +14,22 @@ import org.slf4j.LoggerFactory;
 public class HarriCouchDBDao implements IHarriDAO {
 	private static Logger LOG = LoggerFactory.getLogger(HarriCouchDBDao.class);
 
+	private String username = "";
+	private String password = "";
+	private String url = "";
+	
 	public HarriCouchDBDao() {
 		LOG.info("HarriCouchDBDao constructor");
+		Properties harriConfigs = HarriUtils.getHarriConfigs();
+		username = harriConfigs.getProperty("couchdb.user", "harri");
+		password = harriConfigs.getProperty("couchdb.password", "set.password.here");
+		url = harriConfigs.getProperty("couchdb.url", "https://localhost:6984");
+		boolean checkCouchStatus = checkCouchStatus();
 	}
 	
-	
+	public boolean checkCouchStatus() {
+		RestTemplate rt = new RestTemplate();
+		String response = rt.getForObject(this.url, String.class);
+		return true;
+	}
 }
